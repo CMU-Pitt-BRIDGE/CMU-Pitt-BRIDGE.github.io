2bd3c436e3ac14f1f4b933f5e648e88ad42abef1
eduardojdiniz <edd32@pitt.edu>
2023-10-12 13:30:56 -0400

    Settings for gh-pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,16 +11,16 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://CMU-Pitt-BRIDGE.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
+  projectName: "CMU-Pitt-BRIDGE.github.io", // Usually your repo name.
   organizationName: "CMU-Pitt-BRIDGE", // Usually your GitHub org/user name.
-  projectName: "abridged", // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
