39d0764420616e6217db22d2a97908cdbcbf0b35
eduardojdiniz <edd32@pitt.edu>
2023-10-12 13:57:11 -0400

    Deploy website - based on 2bd3c436e3ac14f1f4b933f5e648e88ad42abef1

---
+++ assets/js/845233a1.551de78e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkabridged=self.webpackChunkabridged||[]).push([[7437],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ assets/js/f8551aab.c77592ac.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkabridged=self.webpackChunkabridged||[]).push([[1743],{7574:(e,a,t)=>{t.r(a),t.d(a,{default:()=>r});var l=t(7961),n=t(7294);function r(){return n.createElement(l.Z,null,n.createElement("h1",null,"My React Page"),n.createElement("p",null,"This is a React page"))}}}]);
